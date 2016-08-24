61b4a17ba568bc23b469cbef1cf49e63e4c195fd
Maksim Rakitin <maxim.rakitin@gmail.com>
2016-08-24 18:36:19 -0400

    Plotting of FWHM for both axes is supported now.

--- fwhm_plot.py
+++ fwhm_plot.py
@@ -12,29 +12,46 @@ import numpy as np
 PICS_DIR = 'pics'
 
 
-def get_x_width(dat_file, y_idx='middle', plot=True, variate_steps=0, show=False, suffix=''):
+def get_width(dat_file, y_idx='middle', plot=True, variate_steps=0, show=False, suffix='', axis='y'):
     list_2d, x, y = prepare_data(dat_file)
 
+    if axis == 'x':
+        idx = 0
+        axis_values = np.copy(x)
+        other_values = np.copy(y)
+        position = 'Vertical'
+    elif axis == 'y':
+        idx = 1
+        axis_values = np.copy(y)
+        other_values = np.copy(x)
+        position = 'Horizontal'
+    else:
+        raise ValueError('"{}" is not a valid option. Use either "x" or "y".'.format(axis))
+
     if y_idx == 'middle':
-        y_idx = np.where(list_2d == list_2d.max())[1][0]
+        y_idx = np.where(list_2d == list_2d.max())[idx][0]
+
     factor = 0.5
     m_to_um = 1e6
 
     widths = []
     for i in range(y_idx - variate_steps, y_idx + variate_steps + 1):
         try:
-            y_coor = y[i] * m_to_um
+            y_coor = axis_values[i] * m_to_um
         except IndexError:
             raise ValueError('Index #{} is beyond the size of the array {}'.format(i, len(y)))
 
-        x_at_zero_y = list_2d[:, i]
+        if axis == 'x':
+            x_at_zero_y = list_2d[i, :]
+        else:
+            x_at_zero_y = list_2d[:, i]
         max_value = x_at_zero_y.max()
         frac_of_max = max_value * factor
         idx_ge_frac_of_max = np.where(x_at_zero_y >= frac_of_max)[0]
-        x_ge_frac_of_max = x[idx_ge_frac_of_max]
+        x_ge_frac_of_max = other_values[idx_ge_frac_of_max]
 
         # Rescaling for plotting:
-        x_um = np.copy(x) * m_to_um
+        x_um = np.copy(other_values) * m_to_um
         x_ge_frac_of_max_um = np.copy(x_ge_frac_of_max) * m_to_um
 
         vals_ge_frac_of_max = x_at_zero_y[idx_ge_frac_of_max]
@@ -49,12 +66,13 @@ def get_x_width(dat_file, y_idx='middle', plot=True, variate_steps=0, show=False
             plt.figure(figsize=(10, 6))
             plt.title(
                 u'Width at {}: {} {}'.format(frac_of_max_text, round(width, 3), units_um))
-            plt.xlabel(u'Horizontal Position [{}]'.format(units_um))
+            plt.xlabel(u'{} Position [{}]'.format(position, units_um))
             plt.ylabel(u'Intensity [ph/s/.1%bw/mm²]')
 
             plt.plot(x_um, x_at_zero_y, label=u'All X values at Y={} {} (index #{})'.format(y_coor, units_um, i))
             plt.plot(x_ge_frac_of_max_um, vals_ge_frac_of_max, c='red', label='Values >= {}'.format(frac_of_max_text))
-            plt.xlim([x_ge_frac_of_max_um[0] * 2, x_ge_frac_of_max_um[-1] * 2])
+            half_width = abs(x_ge_frac_of_max_um[-1] - x_ge_frac_of_max_um[0]) * 0.5
+            plt.xlim([x_ge_frac_of_max_um[0] - half_width, x_ge_frac_of_max_um[-1] + half_width])
             plt.grid()
             plt.axvline(0.0, c='black')
             plt.legend()
@@ -62,7 +80,7 @@ def get_x_width(dat_file, y_idx='middle', plot=True, variate_steps=0, show=False
                 plt.show()
             else:
                 suf = '{}_'.format(suffix) if suffix else ''
-                pic_name = os.path.join(PICS_DIR, 'x_{}{:05d}.png'.format(suf, i))
+                pic_name = os.path.join(PICS_DIR, 'x_{}{:05d}_{}.png'.format(suf, i, axis))
                 plt.savefig(pic_name, dpi=100)
             plt.cla()
             plt.clf()
@@ -120,5 +138,6 @@ def _read_data(dat_file, skip_lines=11):
 if __name__ == '__main__':
     for suffix in ['es1', 'es2']:
         dat_file = 'SMI/me_fwhm/res_int_pr_me_{}.dat'.format(suffix)
-        widths = get_x_width(dat_file, y_idx='middle', variate_steps=1, show=False, suffix=suffix)
-        print('Widths for {}: {}'.format(suffix, widths))
+        for axis in ['x', 'y']:
+            widths = get_width(dat_file, y_idx='middle', variate_steps=0, show=False, suffix=suffix, axis=axis)
+            print('Widths for {} ({}): {}'.format(suffix, axis, widths))
